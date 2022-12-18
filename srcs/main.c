8d89fb905378e14cb9e291c4f5976bdd18a12e4d
zoou2foo <zoou2foo@gmail.com>
2022-12-18 07:36:46 -0500

    checked rl lib works at home

--- srcs/main.c
+++ srcs/main.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
+/*   By: valeriejean <valeriejean@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:40:48 by vjean             #+#    #+#             */
-/*   Updated: 2022/12/17 15:23:11 by vjean            ###   ########.fr       */
+/*   Updated: 2022/12/18 07:34:56 by valeriejean      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	main()
-// {
-// 	char	*buf;
-
-// 	buf = readline("bash-Pew Pew> "); //readline will malloc it
-// 	while (buf)
-// 	{
-// 		if (buf[0])
-// 			add_history(buf);
-// 		printf("%s\n", buf);
-// 		free(buf); //but readline does not free it for us!
-// 		buf = readline("bash-Pew Pew> ");
-// 	}
-// 	clear_history();
-// 	return (0);
-// }
-
-int	main(int ac, char **av, char **envp)
+int	main()
 {
+	char	*buf;
 
+	buf = readline("bash-Pew Pew> "); //readline will malloc it
+	while (buf)
+	{
+		if (buf[0])
+			add_history(buf);
+		printf("%s\n", buf);
+		free(buf); //but readline does not free it for us!
+		buf = readline("bash-Pew Pew> ");
+	}
+	clear_history();
+	return (0);
 }
+
+// int	main(int ac, char **av, char **envp)
+// {
+
+// }
 
 // COMMENT if ac is not 1, error; void argv.

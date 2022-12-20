fcd4c484ad2e2843864a8a27269ca888e81678e2
Valerie <zoou2foo@gmail.com>
2022-12-20 14:30:49 -0500

    end of day

--- srcs/main.c
+++ srcs/main.c
@@ -3,35 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: valeriejean <valeriejean@student.42.fr>    +#+  +:+       +#+        */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:40:48 by vjean             #+#    #+#             */
-/*   Updated: 2022/12/18 07:34:56 by valeriejean      ###   ########.fr       */
+/*   Updated: 2022/12/20 12:25:10 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// int	main()
+// {
+// 	char	*buf;
+
+// 	buf = readline("bash-Pew Pew> "); //readline will malloc it
+// 	while (buf)
+// 	{
+// 		if (buf[0])
+// 			add_history(buf);
+// 		printf("%s\n", buf);
+// 		free(buf); //but readline does not free it for us!
+// 		buf = readline("bash-Pew Pew> ");
+// 	}
+// 	clear_history();
+// 	return (0);
+// }
+
 int	main()
 {
-	char	*buf;
-
-	buf = readline("bash-Pew Pew> "); //readline will malloc it
-	while (buf)
-	{
-		if (buf[0])
-			add_history(buf);
-		printf("%s\n", buf);
-		free(buf); //but readline does not free it for us!
-		buf = readline("bash-Pew Pew> ");
-	}
-	clear_history();
+	char	s[100];
+	
+	printf("%s\n", getcwd(s, 100));
+	chdir("../Evaluation");
+	printf("%s\n", getcwd(s, 100));
 	return (0);
 }
-
-// int	main(int ac, char **av, char **envp)
-// {
-
-// }
 
 // COMMENT if ac is not 1, error; void argv.

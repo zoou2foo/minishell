00d1012c5e166b02629b2c992a8894f4a4fcc432
Valerie <zoou2foo@gmail.com>
2022-12-21 15:32:57 -0500

    end of the day

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 11:27:34 by vjean             #+#    #+#             */
-/*   Updated: 2022/12/17 15:16:48 by vjean            ###   ########.fr       */
+/*   Updated: 2022/12/21 15:32:34 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,20 @@
 typedef struct s_data{
 	char	**envp;
 	char	**copy_envp;
+	char	*buf; //variable pour garder ce qui est mis dans readline
 
 }	t_data;
 
-/* section one */
+/* section one - all about our struct */
+void	init_struct(t_data *data, char **envp);
 
 /* section two */
 
 /* section three */
 
 /* section four */
+
+/* section five - trying stuff */
+void	try_something(t_data *data);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,37 +6,58 @@
 /*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:40:48 by vjean             #+#    #+#             */
-/*   Updated: 2022/12/20 12:25:10 by vjean            ###   ########.fr       */
+/*   Updated: 2022/12/21 11:26:38 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// int	main()
-// {
-// 	char	*buf;
-
-// 	buf = readline("bash-Pew Pew> "); //readline will malloc it
-// 	while (buf)
-// 	{
-// 		if (buf[0])
-// 			add_history(buf);
-// 		printf("%s\n", buf);
-// 		free(buf); //but readline does not free it for us!
-// 		buf = readline("bash-Pew Pew> ");
-// 	}
-// 	clear_history();
-// 	return (0);
-// }
-
-int	main()
+int	main(int ac, char **av, char **envp)
 {
-	char	s[100];
-	
-	printf("%s\n", getcwd(s, 100));
-	chdir("../Evaluation");
-	printf("%s\n", getcwd(s, 100));
+	// char	*buf;
+	t_data	*data;
+
+	(void)av;
+	if (ac == 1)
+	{
+		data = ft_calloc(sizeof(t_data), 1);
+		init_struct(data, envp);
+		data->buf = readline("bash-Pew Pew> ");
+		while (data->buf)
+		{
+			if (data->buf[0])
+				add_history(data->buf);
+			try_something(data);
+			free(data->buf);
+			data->buf = readline("bash-Pew Pew> ");
+		}
+		clear_history();
+	}
 	return (0);
 }
 
+void	init_struct(t_data *data, char **envp)
+{
+	data->envp = envp;
+	data->copy_envp = envp;
+}
+
+void	try_something(t_data *data)
+{
+	printf("trying some stuff\n");
+	printf("%s\n", data->buf);
+	printf("and??");
+}
+// int	main()
+// {
+// 	char	s[100];
+
+// 	printf("%s\n", getcwd(s, 100));
+// 	chdir("../Evaluation");
+// 	printf("%s\n", getcwd(s, 100));
+// 	return (0);
+// }
+
 // COMMENT if ac is not 1, error; void argv.
+// COMMENT readline will malloc the char *buf, but it does NOT free it at the
+// COMMENT end.

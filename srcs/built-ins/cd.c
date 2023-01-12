af9ea4bd56b327b2d96c1310076007192224cf9b
Valerie <zoou2foo@gmail.com>
2023-01-12 11:00:54 -0500

    some notes

--- srcs/built-ins/cd.c
+++ srcs/built-ins/cd.c
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 12:49:38 by vjean             #+#    #+#             */
-/*   Updated: 2023/01/12 10:38:19 by llord            ###   ########.fr       */
+/*   Updated: 2023/01/12 10:58:22 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	change_dir(t_meta *metadata)
+void	change_dir(t_meta *metadata) //il ajouter t_cmd *cmd;
 {
 	char	dir[100];
 
 	getcwd(dir, sizeof(dir));
-	if (chdir(argument) != 0)		//placeholder variable (?)
+	if (chdir(argument) != 0)		//placeholder variable (?) **cmd_args
 	{
 		write(2, "Error: no such file or directory: argument", 42);
 		exit (1);

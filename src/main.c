c8788d6bb346efaba629fdfa3ba07ff0862b5485
Valerie <zoou2foo@gmail.com>
2023-01-20 15:27:34 -0500

    before exam

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 14:40:48 by vjean             #+#    #+#             */
-/*   Updated: 2023/01/20 12:58:50 by llord            ###   ########.fr       */
+/*   Updated: 2023/01/20 13:16:53 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,7 +60,7 @@ void	init_meta(void)
 */
 // COMMENT if ac is not 1, error; void argv.
 // COMMENT readline will malloc the char *buf, but it does NOT free it at the end.
-/*
+
 //VAL's main (DEBUG)
 int	main(void)
 {
@@ -69,67 +69,67 @@ int	main(void)
 	printf("%s\n", do_expand_in_dbl_quotes(str));
 	return (0);
 }
-*/
+
 
 
 //Loyc's main (DEBUG)
 
-int	main(void)
-{
-	bool	show_joined = true;
-	bool	show_newline = true;
-	bool	show_content = true;
-	bool	show_types = false;
+// int	main(void)
+// {
+// 	bool	show_joined = true;
+// 	bool	show_newline = true;
+// 	bool	show_content = true;
+// 	bool	show_types = false;
 
-	char	*line = "<<END <$HOMEinfile grep -v 42 | >> outfile wc -l > outfile2 | ls | >outfile3 | echo \"don't | split\"";
-	//char	*line = "lol \"L O L\"\"lol\"lol\'L O L\'lol";
-	//char	*line = "\"s\"p\'l\'";
-	//char	*line = "$USER$USER";
+// 	char	*line = "<<END <$HOMEinfile grep -v 42 | >> outfile wc -l > outfile2 | ls | >outfile3 | echo \"don't | split\"";
+// 	//char	*line = "lol \"L O L\"\"lol\"lol\'L O L\'lol";
+// 	//char	*line = "\"s\"p\'l\'";
+// 	//char	*line = "$USER$USER";
 
-	t_token	*node;
+// 	t_token	*node;
 
- 	printf("Tokenizing Line...\n");
- 	node = tokenize_input(line);
- 	printf("Printing Tokens...\n\n");
+//  	printf("Tokenizing Line...\n");
+//  	node = tokenize_input(line);
+//  	printf("Printing Tokens...\n\n");
 
-	while (node)
-	{
-		if (node && show_joined && node->is_joined)
-				printf("__");
-		else if (show_newline)
-			printf("\n");
-		else
-			printf(" ");
+// 	while (node)
+// 	{
+// 		if (node && show_joined && node->is_joined)
+// 				printf("__");
+// 		else if (show_newline)
+// 			printf("\n");
+// 		else
+// 			printf(" ");
 
-		if (node->type < 5)
-		{
-			if (show_content)
-				printf("%s", node->string);
-			else if (node->type == 1)
-				printf("string");
-			else if (node->type == 2)
-				printf("\'string\'");
-			else if (node->type == 3)
-				printf("\"string\"");
-			else if (node->type == 4)
-				printf("$EXPAND");
-		}
-		else
-		{
-			if (show_types)
-				printf("_T%i_", node->type);
-			else if (node->type == 5)
-				printf("<");
-			else if (node->type == 6)
-				printf("<<");
-			else if (node->type == 7)
-				printf(">");
-			else if (node->type == 8)
-				printf(">>");
-			else if (node->type == 9)
-				printf("|");
-		}
-		node = node->next;
-	}
-	printf("\n\n");
-}
+// 		if (node->type < 5)
+// 		{
+// 			if (show_content)
+// 				printf("%s", node->string);
+// 			else if (node->type == 1)
+// 				printf("string");
+// 			else if (node->type == 2)
+// 				printf("\'string\'");
+// 			else if (node->type == 3)
+// 				printf("\"string\"");
+// 			else if (node->type == 4)
+// 				printf("$EXPAND");
+// 		}
+// 		else
+// 		{
+// 			if (show_types)
+// 				printf("_T%i_", node->type);
+// 			else if (node->type == 5)
+// 				printf("<");
+// 			else if (node->type == 6)
+// 				printf("<<");
+// 			else if (node->type == 7)
+// 				printf(">");
+// 			else if (node->type == 8)
+// 				printf(">>");
+// 			else if (node->type == 9)
+// 				printf("|");
+// 		}
+// 		node = node->next;
+// 	}
+// 	printf("\n\n");
+// }
--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -3,77 +3,13 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llord <llord@student.42.fr>                +#+  +:+       +#+        */
+/*   By: vjean <vjean@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:06:47 by llord             #+#    #+#             */
-/*   Updated: 2023/01/17 16:12:16 by llord            ###   ########.fr       */
+/*   Updated: 2023/01/20 13:21:41 by vjean            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minishell.h"
 
-t_meta	*metadata;	//our global var
-/*
-
-//=========( CMDER )=========//
-
-//=========( NODER )=========//
-
-t_cmd	*parse_cmd(char *cmdstr)
-{
-
-}
-*/
-
-//========( BLOCKER )========//
-
-//appends cmdnode(s) to the given cmdblock (normally one except if heredoc)
-void	add_cmds(void)//t_cmd_block *cmdblock, char *cmdstr)
-{
-	//TOKENIZE FIRST ARGGGG
-
-	/*
-	<< < << < cmd args >> > >>
-
-	look for heredocs and append them as cmds, then discard them
-	(empty cmd with is_heredoc and has_outpipe set to true)
-	also remove redirections before heredocs
-	( << | << | ) < cmd args >> > >>
-
-	set input and output to last encountered values and discard them
-	cmd args
-
-	set remainder as cmd_args (keep ""/'' intact and aply changes in "${}")
-		(use parse cmd)
-	*/
-
-}
-
-
-//splits the cmd line along pipes and
-t_cmd_block	*parse_line(char *line)
-{
-	t_cmd_block	*cmdblock;
-	char		**cmdstrs;
-	int			i;
-
-	cmdblock = ft_calloc(sizeof(t_cmd_block), 1);
-
-	if (line && line[0])
-	{
-		cmdstrs = ft_split(line, '|');
-		cmdblock->cmd_count = ft_count_char(line, '|') + 1;
-		//remember to update cmd_count when dealing with heredocs later
-
-		i = -1;
-		while (cmdstrs[++i])
-		{
-			add_cmds();//cmdblock, cmdstrs[i]);
-		}
-		cmdblock->cmds = ft_calloc(cmdblock->cmd_count + 1, sizeof(t_cmd *));
-
-		//fill cmds with the cmds in the cmdnode list
-	}
-	return (cmdblock);
-}
+void	create_cmds_lst()
